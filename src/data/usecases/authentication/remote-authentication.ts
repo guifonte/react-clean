0609dc6aa6dd776c4cd01434c06bd4edbeed8c03
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-03-27 01:37:44 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,6 +1,6 @@
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
-import { InvalidCredentialsError } from '@/domain/errors/invalid-credential-errors'
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 
 export class RemoteAuthentication {
