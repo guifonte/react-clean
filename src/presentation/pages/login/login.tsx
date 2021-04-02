2149e180a22970307390f4c03c2b321eafbc79f9
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-04-02 13:05:15 -0300

    feat: ensure Login navigates to signup correctly

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import Styles from './login-styles.scss'
-import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
-import Context from '@/presentation/contexts/form/form-context'
-import { Validation } from '@/presentation/protocols/validation'
 import { Authentication } from '@/domain/usecases'
+import Context from '@/presentation/contexts/form/form-context'
+import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
+import { Validation } from '@/presentation/protocols/validation'
 
 type Props = {
   validation: Validation
@@ -61,7 +62,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
           <Input type="email" name="email" placeholder="Digite o seu e-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
           <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} className={Styles.submit} type="submit">Entrar</button>
-          <span className={Styles.link}>Criar conta</span>
+          <Link data-testid="signup" to="/signup"className={Styles.link}>Criar conta</Link>
           <FormStatus />
         </form>
       </Context.Provider>
