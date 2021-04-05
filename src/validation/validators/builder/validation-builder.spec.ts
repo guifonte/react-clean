7d399555bc01ecac8b491e64fc2f47f5d2e40931
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-04-05 17:56:40 -0300

    test: ensure ValidationBuilder returns a EmailValidation

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,9 +1,15 @@
 import { RequiredFieldValidation } from '@/validation/validators'
+import { EmailValidation } from '../email/email-validation'
 import { ValidationBuilder as sut } from './validation-builder'
 
 describe('ValidationBuilder', () => {
   test('Should return a RequiredFieldValidation', () => {
     const fields = sut.field('any_field').required().build()
     expect(fields).toEqual([new RequiredFieldValidation('any_field')])
+  })
+
+  test('Should return a EmailValidation', () => {
+    const fields = sut.field('any_field').email().build()
+    expect(fields).toEqual([new EmailValidation('any_field')])
   })
 })
