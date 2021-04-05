f2b539ed589a3af3e8c0a1fb87e6f645edf3518d
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2021-04-05 18:03:40 -0300

    refactor: change fields to validators

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -5,17 +5,17 @@ import { ValidationBuilder as sut } from './validation-builder'
 
 describe('ValidationBuilder', () => {
   test('Should return a RequiredFieldValidation', () => {
-    const fields = sut.field('any_field').required().build()
-    expect(fields).toEqual([new RequiredFieldValidation('any_field')])
+    const validators = sut.field('any_field').required().build()
+    expect(validators).toEqual([new RequiredFieldValidation('any_field')])
   })
 
   test('Should return a EmailValidation', () => {
-    const fields = sut.field('any_field').email().build()
-    expect(fields).toEqual([new EmailValidation('any_field')])
+    const validators = sut.field('any_field').email().build()
+    expect(validators).toEqual([new EmailValidation('any_field')])
   })
 
   test('Should return a MinLengthValidation', () => {
-    const fields = sut.field('any_field').min(5).build()
-    expect(fields).toEqual([new MinLengthValidation('any_field', 5)])
+    const validators = sut.field('any_field').min(5).build()
+    expect(validators).toEqual([new MinLengthValidation('any_field', 5)])
   })
 })
